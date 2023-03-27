c55635229d6cb48a3be0b7305fb0bae90ddb3f39
Florian Metz <me@timeraa.dev>
2023-03-27 19:59:27 +0200

    wip: statuspage

---
+++ Docker/statuspage.dockerfile
@@ -0,0 +1,23 @@
+FROM node:lts-alpine
+
+WORKDIR /app
+
+RUN corepack enable
+
+COPY pnpm-lock.yaml ./
+RUN pnpm fetch
+
+COPY . ./
+RUN pnpm --filter=statuspage build
+RUN pnpm --filter=statuspage --prod deploy pruned
+
+FROM node:lts-alpine
+
+WORKDIR /app
+
+ENV NODE_ENV=production
+
+COPY --from=0 /app/pruned/node_modules ./node_modules
+COPY --from=0 /app/pruned/dist ./
+
+CMD ["node", "index.js"]
---
+++ apps/statuspage/src/index.ts
@@ -0,0 +1,65 @@
+import { CronJob } from "cron";
+import debug from "debug";
+import ky, { HTTPError } from "ky";
+import StatuspageApi from "statuspage.ts";
+
+if (process.env.NODE_ENV === undefined) {
+	const { config } = await import("dotenv");
+
+	config({ path: "../../../.env" });
+}
+
+const log = debug("statuspage");
+debug.enable("statuspage*");
+
+if (process.env.STATUSPAGE_API_TOKEN === undefined)
+	throw new Error("STATUSPAGE_API_TOKEN is not defined in .env file!");
+if (process.env.STATUSPAGE_PAGE_ID === undefined)
+	throw new Error("STATUSPAGE_PAGE_ID is not defined in .env file!");
+
+const statuspage = new StatuspageApi(process.env.STATUSPAGE_API_TOKEN);
+
+let pingResultsLast5Minutes: { timestamp: number; value: number }[] = [];
+
+new CronJob("*/30 * * * * *", async () => {
+	try {
+		let duration = Date.now();
+		await ky.get("https://api.premid.app/v3?query=%7B__typename%7D", {
+			timeout: 5000,
+		});
+
+		duration = Date.now() - duration;
+		pingResultsLast5Minutes.push({
+			timestamp: Math.floor(Date.now() / 1000),
+			value: duration,
+		});
+
+		log("Successfully pinged API! %dms", duration);
+	} catch (error) {
+		const error1 = error as HTTPError;
+
+		log(
+			"Failed to ping API! %d %s",
+			error1.response.status,
+			error1.response.statusText
+		);
+	}
+}).start();
+
+new CronJob("*/5 * * * *", async () => {
+	const results = pingResultsLast5Minutes;
+	pingResultsLast5Minutes = [];
+
+	//TODO It's down.
+	if (results.length === 0) return;
+
+	const result = await statuspage.metrics.addDataPoints(
+		process.env.STATUSPAGE_PAGE_ID || "",
+		{
+			"21gnbqvy6126": results,
+		}
+	);
+
+	if (result.isOk) log("Successfully submitted metric data!");
+	else log("Failed to submit metric data! %O", result.error);
+}).start();
